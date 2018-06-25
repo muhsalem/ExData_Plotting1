bb1aa8e8cb2cd0d688dc88e01a16c4b356527ae0
muhsalem <36573776+muhsalem@users.noreply.github.com>
2018-06-25 22:02:00 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,7 +1,7 @@
 > #Reading, naming and subsetting power consumption data
-> power <- read.table("household_power_consumption.txt",skip=1,sep=";")
-> names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
-> subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+> DataPower <- read.table("household_power_consumption.txt",skip=1,sep=";")
+> names(DataPower) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+> subpower <- subset(DataPower,DataPower$Date=="1/2/2007" | DataPower$Date =="2/2/2007")
 > 
 > #calling the basic plot function
 > hist(as.numeric(as.character(subpower$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
